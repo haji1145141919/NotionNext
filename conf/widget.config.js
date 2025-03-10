976f2ba71381a5cf4897f8af99df1075f94a5dae
haji1145141919 <haji1145141919@outlook.com>
2025-03-10 15:51:26 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -44,11 +44,11 @@ module.exports = {
         'https://p1.music.126.net/bU_UznrJaqyaSbaaQFZCKw==/109951165687192876.jpg'
     },
     {
-      name: '王都グランセル',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
+      name: 'フォールオブフォール　~ 秋めく滝',
+      artist: 'logical emotion',
+      url: 'https://music.163.com/song/media/outer/url?id=776196.mp3',
       cover:
-        'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+        'https://p1.music.126.net/lebnxqn4hlb-_6xqYkMYuA==/885106860359941.jpg'
     }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
